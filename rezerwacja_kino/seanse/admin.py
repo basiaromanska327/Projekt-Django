329b1e95b125b6001684500a9f2f03f8c3be00c1
basiaromanska327 <basiaromanska327@gmail.com>
2024-12-15 15:54:03 +0100

    formatowanie klas

--- rezerwacja_kino/seanse/admin.py
+++ rezerwacja_kino/seanse/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from seanse.models import Film, Sala, Seans
 # Register your models here.
+admin.site.register(Film)
+admin.site.register(Sala)
+admin.site.register(Seans)
--- rezerwacja_kino/seanse/models.py
+++ rezerwacja_kino/seanse/models.py
@@ -5,12 +5,18 @@ class Film(models.Model):
     nazwa = models.CharField(verbose_name="Tytul filmu", max_length=64)
     plakat = models.ImageField(verbose_name="Plakat")
 
+    class Meta:
+        verbose_name_plural = "Filmy"
+
     def __str__(self):
         return f"Film {self.nazwa}"    
 
 class Sala(models.Model):
     nazwa = models.CharField(verbose_name="Nazwa sali", max_length=32)
     liczba_miejsc = models.IntegerField(verbose_name="Liczba miejsc")
+
+    class Meta:
+        verbose_name_plural = "Sale"
 
     def __str__(self):
         return f"Sala {self.nazwa}"
@@ -19,3 +25,10 @@ class Seans(models.Model):
     film = models.ForeignKey(Film, on_delete=models.PROTECT)
     termin = models.DateTimeField(verbose_name="Termin")
     sala = models.ForeignKey(Sala, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name_plural = "Seanse"
+
+    def __str__(self):
+        return f"Seans {self.film.nazwa} {self.termin.strftime("%Y-%m-%d %H:%M")} Sala {self.sala.nazwa}"
+    
