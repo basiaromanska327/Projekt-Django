4462c1db62ff1c6f3bae9cf2b088ca9a1f8d1293
basiaromanska327 <basiaromanska327@gmail.com>
2024-12-21 19:11:33 +0100

    stworzenie widoku seansow

--- rezerwacja_kino/seanse/views.py
+++ rezerwacja_kino/seanse/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Seans
 
-# Create your views here.
+class SeanseListView(ListView):
+    model = Seans
+    template_name = "lista_seansow.html"
+
+class SeanseDetailView(DetailView):
+    model = Seans
+    template_name = "szczegoly_seansow.html"
+    
+
+
