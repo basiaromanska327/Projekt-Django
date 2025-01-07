14bd00a4c6c695972fe95b5848f4d54df568cab2
basiaromanska327 <basiaromanska327@gmail.com>
2025-01-07 20:30:42 +0100

    dodanie podstrony z rezerwacjami

--- rezerwacja_kino/seanse/views.py
+++ rezerwacja_kino/seanse/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Seans
+from .models import Seans, Rezerwacja
+from django.shortcuts import get_object_or_404, redirect
 
 class SeanseListView(ListView):
     model = Seans
@@ -9,6 +10,19 @@ class SeanseListView(ListView):
 class SeanseDetailView(DetailView):
     model = Seans
     template_name = "szczegoly_seansow.html"
+
+class RezerwacjaDetailView(DetailView):
+    model = Rezerwacja
+    template_name = "szczegoly_rezerwacji.html"
     
-
-
+def create_reservation(request, pk, bilety):
+    if request.action =="POST" and request.user.is_authenticated:
+        seans = get_object_or_404(Seans, pk=pk)
+        if seans.liczba_wolnych_miejsc() >= bilety:
+            rezerwacja = Rezerwacja.object.create(
+                user=request.user,
+                seans=seans,
+                liczba_biletow=bilety
+            )
+            return redirect(f"rezerwacje/{rezerwacja.id}")
+        return redirect(f"seans/{seans.id}")
